cc7a780906969172aa8e883a24041dfc0cd633c8
Maggie Dreyer <maggie@puppet.com>
2018-12-01 14:36:51 -0800

    Add clj-http lib

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject advent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solutions to the 2018 Advent of Code"
+  :url "https://github.com/magisus/adventofcode2018"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "0.7.7"]]
+
   :main ^:skip-aot advent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
